1cd8efa1199bfcb77502ccd37693f7b75382dd57
laysleal <laysleal13@gmail.com>
2024-05-15 21:05:25 -0300

    terceiro commit

--- classificador.js
+++ classificador.js
@@ -1,27 +1,35 @@
-let nome = prompt("Digite o nome do herói:");
+while (true) {
 
-// Definindo a variável para armazenar a quantidade de experiência (XP) do herói
-let xp = parseInt(prompt("Digite a quantidade de experiência (XP) do herói:"));
+    let nome = prompt("Digite o nome do herói:");
 
-// Determinando o nível do herói com base na quantidade de XP
-let nivel;
-if (xp < 1000) {
-    nivel = "Ferro";
-} else if (xp >= 1001 && xp <= 2000) {
-    nivel = "Bronze";
-} else if (xp >= 2001 && xp <= 5000) {
-    nivel = "Prata";
-} else if (xp >= 5001 && xp <= 7000) {
-    nivel = "Ouro";
-} else if (xp >= 7001 && xp <= 8000) {
-    nivel = "Platina";
-} else if (xp >= 8001 && xp <= 9000) {
-    nivel = "Ascendente";
-} else if (xp >= 9001 && xp <= 10000) {
-    nivel = "Imortal";
-} else {
-    nivel = "Radiante";
+    if (nome.toLowerCase() === 'sair') {
+        break;
+    }
+
+    // Definindo a variável para armazenar a quantidade de experiência (XP) do herói
+    let xp = parseInt(prompt("Digite a quantidade de experiência (XP) do herói:"));
+
+    // Determinando o nível do herói com base na quantidade de XP
+    let nivel;
+    if (xp < 1000) {
+        nivel = "Ferro";
+    } else if (xp >= 1001 && xp <= 2000) {
+        nivel = "Bronze";
+    } else if (xp >= 2001 && xp <= 5000) {
+        nivel = "Prata";
+    } else if (xp >= 5001 && xp <= 7000) {
+        nivel = "Ouro";
+    } else if (xp >= 7001 && xp <= 8000) {
+        nivel = "Platina";
+    } else if (xp >= 8001 && xp <= 9000) {
+        nivel = "Ascendente";
+    } else if (xp >= 9001 && xp <= 10000) {
+        nivel = "Imortal";
+    } else {
+        nivel = "Radiante";
+    }
+
+    // Exibindo a mensagem final
+    console.log(`O Herói ${nome} está no nível de ${nivel}`);
+
 }
-
-// Exibindo a mensagem final
-console.log(`O Herói ${nome} está no nível de ${nivel}`);
